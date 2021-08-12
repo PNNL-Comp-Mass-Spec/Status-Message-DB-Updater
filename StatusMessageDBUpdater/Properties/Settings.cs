9e4759c261b95d2619d3b50d86334fa4baa74ec5
alchemistmatt <matthew.monroe@pnnl.gov>
2021-08-12 13:43:27 -0700

    Comment out unused code and update whitespace

--- StatusMessageDBUpdater/Properties/Settings.cs
+++ StatusMessageDBUpdater/Properties/Settings.cs
@@ -1,4 +1,5 @@
-﻿namespace StatusMessageDBUpdater.Properties {
+﻿namespace StatusMessageDBUpdater.Properties
+{
     /// <summary>
     /// This class allows you to handle specific events on the settings class:
     /// <para>The SettingChanging event is raised before a setting's value is changed</para>
@@ -6,22 +7,27 @@
     /// <para>The SettingsLoaded event is raised after the setting values are loaded</para>
     /// <para>The SettingsSaving event is raised before the setting values are saved</para>
     /// </summary>
-    internal sealed partial class Settings {
-        public Settings() {
-            // // To add event handlers for saving and changing settings, uncomment the lines below:
-            //
-            // this.SettingChanging += this.SettingChangingEventHandler;
-            //
-            // this.SettingsSaving += this.SettingsSavingEventHandler;
-            //
-        }
+    internal sealed partial class Settings
+    {
 
-        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
-            // Add code to handle the SettingChangingEvent event here.
-        }
+        // To add event handlers for saving and changing settings, uncomment the constructor and the event handlers
 
-        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e) {
-            // Add code to handle the SettingsSaving event here.
-        }
+        // /// <summary>
+        // /// Constructor
+        // /// </summary>
+        // public Settings() {
+        //     this.SettingChanging += this.SettingChangingEventHandler;
+        //     this.SettingsSaving += this.SettingsSavingEventHandler;
+        // }
+
+        // private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e)
+        // {
+        //     // Add code to handle the SettingChangingEvent event here.
+        // }
+
+        // private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
+        // {
+        //     // Add code to handle the SettingsSaving event here.
+        // }
     }
 }
