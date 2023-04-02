135e013da9586b261b0bbc2d7dadda6d8447b30a
alchemistmatt <matthew.monroe@pnnl.gov>
2023-04-01 18:44:57 -0700

    Add status update procedure name setting

--- StatusMessageDBUpdater/Properties/Settings.Designer.cs
+++ StatusMessageDBUpdater/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace StatusMessageDBUpdater.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -74,6 +74,15 @@ namespace StatusMessageDBUpdater.Properties {
         public string MaxRunTimeHours {
             get {
                 return ((string)(this["MaxRunTimeHours"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("update_manager_and_task_status_xml")]
+        public string StatusUpdateProcedureName {
+            get {
+                return ((string)(this["StatusUpdateProcedureName"]));
             }
         }
     }
