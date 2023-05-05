94288a48a75e35b93145ecf7f39f581b5a98e887
alchemistmatt <matthew.monroe@pnnl.gov>
2023-05-04 19:20:06 -0700

    Change the return parameter direction to InputOutput and use DBToolsBase.GetReturnCode() to get the return code value for the stored procedure

--- StatusMessageDBUpdater/DBAccess.cs
+++ StatusMessageDBUpdater/DBAccess.cs
@@ -22,6 +22,9 @@ namespace StatusMessageDBUpdater
         /// <summary>
         /// Post message to database using stored procedure update_manager_and_task_status_xml or update_capture_task_manager_and_task_status_xml
         /// </summary>
+        /// <remarks>
+        /// The stored procedure name is defined in file StatusMessageDBUpdater.exe.local.config
+        /// </remarks>
         /// <param name="statusMessages"></param>
         /// <param name="procedureName">Procedure to call</param>
         /// <param name="result"></param>
@@ -32,16 +35,26 @@ namespace StatusMessageDBUpdater
             {
                 var cmd = mDBTools.CreateCommand(procedureName, CommandType.StoredProcedure);
 
-                mDBTools.AddParameter(cmd, "@Return", SqlType.Int, ParameterDirection.ReturnValue);
-                mDBTools.AddParameter(cmd, "@managerStatusXML", SqlType.Text).Value = statusMessages.ToString();
-                var messageParam = mDBTools.AddParameter(cmd, "@message", SqlType.VarChar, 4096, ParameterDirection.Output);
+                // Define parameter for procedure's return value
+                // If querying a Postgres DB, mDBTools will auto-change "@return" to "_returnCode"
+                var returnParam = mDBTools.AddParameter(cmd, "@return", SqlType.Int, ParameterDirection.ReturnValue);
 
-                var returnCode = mDBTools.ExecuteSP(cmd);
+                mDBTools.AddParameter(cmd, "@managerStatusXML", SqlType.Text).Value = statusMessages.ToString();
+                mDBTools.AddParameter(cmd, "@infoLevel", SqlType.TinyInt).Value = 0;
+
+                var messageParam = mDBTools.AddParameter(cmd, "@message", SqlType.VarChar, 4096, ParameterDirection.InputOutput);
+
+                // Execute the stored procedure
+                mDBTools.ExecuteSP(cmd);
+
+                // The return code is an integer on SQL Server, but is text on Postgres
+                // Use GetReturnCode to obtain the integer, or find the first integer in the text-based return code
+                var returnValue = DBToolsBase.GetReturnCode(returnParam);
 
                 // Get output parameter value
                 result = mDBTools.GetString(messageParam.Value);
 
-                if (returnCode == 0)
+                if (returnValue == 0)
                 {
                     return true;
                 }
